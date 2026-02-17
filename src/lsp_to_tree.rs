f8cd5a0e10150a2ff9b0b233a90adde5ded9daf6
R. Bramaditya <bramadityaw@duck.com>
2026-02-17 22:05:55 +0700

    refactor: remove unused module

--- src/lsp_to_tree.rs
+++
@@ -1,9 +0,0 @@
-use tower_lsp::lsp_types;
-
-pub fn point(lsp_pos: lsp_types::Position) -> tree_sitter::Point {
-    let lsp_types::Position { line, character } = lsp_pos;
-    tree_sitter::Point {
-        row: line as usize,
-        column: character as usize,
-    }
-}
