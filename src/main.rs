9e337dbf774561bfd41baa992c0ce4f9fe1d6a44
R. Bramaditya <bramadityaw@duck.com>
2026-02-19 09:52:22 +0700

    feat: disable ansi escape sequences and use log file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .with_writer(std::io::stderr)
-        .finish();
+    let dir = std::env::temp_dir();
+    eprintln!("file logging at directory: {dir:?}");
 
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    let file_appender = tracing_appender::rolling::daily(dir, "blase.log");
+    let (file_appender, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_max_level(Level::INFO)
+        .with_writer(file_appender)
+        .init();
 
     // Prefer truly asynchronous piped stdin/stdout without blocking tasks.
     #[cfg(unix)]
