9d9ad9863c4d3f4572ae117ed2ee43cd926e3813
R. Bramaditya <bramadityaw@duck.com>
2026-06-14 16:47:03 +0700

    test: signature help for attributes starting with : (expression attributes)

--- crates/blase/src/analysis/signature_help.rs
+++ crates/blase/src/analysis/signature_help.rs
@@ -63,8 +63,6 @@ impl Component {
         doc: &ParsedDocument,
     ) -> Option<usize> {
         let attr_name = match attr {
-            ast::blade::Attribute::BladeAttribute(_) |
-            ast::blade::Attribute::PhpStatement(_) => None,
             ast::blade::Attribute::ExpressionAttribute(expression_attribute) => expression_attribute.children(&mut expression_attribute.walk()).filter_map(|ch| {
                 let ch = ch.ok()?;
                 match ch {
@@ -81,8 +79,21 @@ impl Component {
                 }
             }).last(),
             ast::blade::Attribute::ShortAttribute(short_attribute) => short_attribute.variable_name().ok().map(Node::upcast),
+            ast::blade::Attribute::BladeAttribute(_) |
+            ast::blade::Attribute::PhpStatement(_) |
+            ast::blade::Attribute::Comment(_) |
+            ast::blade::Attribute::Conditional(_) |
+            ast::blade::Attribute::Envoy(_) |
+            ast::blade::Attribute::InlineDirective(_) |
+            ast::blade::Attribute::Keyword(_) |
+            ast::blade::Attribute::Livewire(_) |
+            ast::blade::Attribute::Loops(_) |
+            ast::blade::Attribute::Props(_) |
+            ast::blade::Attribute::Switch(_) |
+            ast::blade::Attribute::WireUi(_) => None,
         }?;
         let attr_name = doc.text_for_node(db, attr_name)?;
+        let attr_name = attr_name.strip_prefix(':').unwrap_or(attr_name);
         self.attrs(db)?
             .iter()
             .position(|attr| attr.name.as_str() == attr_name)
@@ -110,4 +121,68 @@ fn signature_help_for_attr(
     }
     res.signature.push('>');
     Some(res)
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::LazyLock;
+
+    use expect_test::{Expect, expect};
+
+    use crate::analysis::fixture;
+
+    use super::*;
+
+    const TEST_CONFIG: LazyLock<Config> = LazyLock::new(|| Config {
+        capabilities: async_lsp::lsp_types::ClientCapabilities::default(),
+        workspace_folder: camino::Utf8PathBuf::from_path_buf(std::path::PathBuf::from("/"))
+            .unwrap(),
+        client_info: None,
+    });
+
+    fn check(blase_fixture: &str, expect: Expect) {
+        let config = &TEST_CONFIG;
+        let (analysis, position) = fixture::position(blase_fixture);
+        let help = analysis.signature_help(config, dbg!(position)).unwrap();
+        let actual = match dbg!(help) {
+            Some(help) => match help.active_parameter {
+                Some(active_param) => {
+                    let mut rendered = String::new();
+                    macros::format_to!(rendered, "{}\n", help.signature);
+                    let active_range = help.parameter_ranges()[active_param];
+                    for i in 0..help.signature.len() {
+                        let marker = if active_range.contains_inclusive((i as u32).into()) {
+                            '^'
+                        } else {
+                            '-'
+                        };
+                        rendered.push(marker);
+                    }
+                    rendered
+                }
+                None => String::new(),
+            },
+            None => String::new(),
+        };
+        expect.assert_eq(&actual);
+    }
+
+    #[test]
+    fn named_attribute_signature_help() {
+        check(
+            r#"
+//- /resources/views/components/index-table.blade.php
+@props(['subject', 'rows' => [], 'columns' => []])
+
+//- /resources/views/index.blade.php
+<x-index-table :ro$0ws="$units" subject="unit" :columns="[
+    'name',
+]">
+</x-index-table>
+            "#,
+            expect![[r#"
+                <x-index-table subject="" rows="[]" columns="[]">
+                --------------------------^^^^^^^^^^-------------"#]],
+        );
+    }
 }
